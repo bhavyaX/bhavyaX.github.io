8909ce6665d4f3e9c0718c77e08be26d49dbe82e
bhavyaX <kshitiz.gzb@gmail.com>
2019-09-01 15:58:21 +0530

    added Files

---
+++ precache-manifest.32e85e08b4f18e68c02aa46b8e97ae11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0a9d2f289dc337622cd7405d7c3f72e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "223b8bfa870c55d5fbde",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "da9ac35cd7980f47cef9",
+    "url": "/static/css/main.7a428fa4.chunk.css"
+  },
+  {
+    "revision": "223b8bfa870c55d5fbde",
+    "url": "/static/js/2.9e5873c0.chunk.js"
+  },
+  {
+    "revision": "da9ac35cd7980f47cef9",
+    "url": "/static/js/main.5aa6c756.chunk.js"
+  },
+  {
+    "revision": "237a9e8df6f28a73d75a",
+    "url": "/static/js/runtime~main.5887c839.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
